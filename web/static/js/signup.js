27e22cf008b6a1250bc0a2cfae1f5d1005972e95
gtsop <>
2015-01-26 18:30:22 +0200

    Add insurance field

--- web/static/js/signup.js
+++ web/static/js/signup.js
@@ -4,11 +4,12 @@ $(function () {
     $('#sign-up').click(function () {
         
         var amka = $('#amka').val();
+        var insurance = $('#insurance').val();
         var firstName = $('#first-name').val();
         var lastName = $('#last-name').val();
         var password = $('#password').val();
         
-        if (!amka || !firstName || !lastName || !password) {
+        if (!amka || !insurance || !firstName || !lastName || !password) {
             showError("Fill in all fields");
             return;
         }
@@ -20,6 +21,7 @@ $(function () {
             type: 'POST',
             data: {
                 amka: amka,
+                insurance: insurance,
                 firstName: firstName,
                 lastName: lastName,
                 password: password
